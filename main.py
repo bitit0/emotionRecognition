ae3782ea29c238f615dcff4a5df76b9f91d3d709
bitit0 <legacygamer331@gmail.com>
2022-11-14 22:39:50 -0500

    small clean up and comments

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os
 import scipy
 import tensorflow as tf
 
-print(tf.__version__)
 
+## data validation test
 
 # (0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral)
 
@@ -42,8 +42,8 @@ trainingGenerator = trainingDataGenerator.flow_from_directory(
     train,
     target_size=(48, 48),
     batch_size=64,
-    color_mode="grayscale",
-    class_mode="categorical"
+    color_mode="grayscale", # grayscale to simplify it
+    class_mode="categorical" # there are 6 outputs for the 6 emotions, so categorical is best
 )
 
 validationDataGenerator = ImageDataGenerator(rescale=1/.255)
@@ -55,22 +55,26 @@ validationGenerator = validationDataGenerator.flow_from_directory(
     class_mode="categorical"
 )
 
-# define model
+# define convolutional neural network model
 
 emotRecModel = Sequential()
-emotRecModel.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotRecModel.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))            # common base, 32 filters, 3x3 kernel size, > 0 outputs
 emotRecModel.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-emotRecModel.add(MaxPooling2D(pool_size=(2, 2)))
-emotRecModel.add(Dropout(0.25))
+emotRecModel.add(MaxPooling2D(pool_size=(2, 2)))                                                        # reduce spatial dimensions
+emotRecModel.add(Dropout(0.25))                                                                         # prevents overfitting
+
 emotRecModel.add(Conv2D(128, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
 emotRecModel.add(MaxPooling2D(pool_size=(2, 2)))
 emotRecModel.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 emotRecModel.add(MaxPooling2D(pool_size=(2, 2)))
 emotRecModel.add(Dropout(0.25))
-emotRecModel.add(Flatten())
+
+emotRecModel.add(Flatten())                                                                             # flattens into vector
 emotRecModel.add(Dense(1024, activation='relu'))
 emotRecModel.add(Dropout(0.5))
+
 emotRecModel.add(Dense(7, activation="softmax"))
+
 plot_model(emotRecModel, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
 emotRecModel.summary()
 
